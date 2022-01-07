f4651cfedce51b1083b2973a52a7ef3e70dcefc3
truong99 <truongnguyen30999@gmail.com>
2022-01-07 08:56:39 +0700

    deloy

--- light-bootstrap-dashboard-react-master/src/utility/config.js
+++ light-bootstrap-dashboard-react-master/src/utility/config.js
@@ -1,10 +1,10 @@
 export const userProfile = {
-    userId:"",
+    userId: "",
     Email: "",
-    accessToken:"",
-    userName:""
+    accessToken: "",
+    userName: ""
 }
-export const URL = "http://3.15.7.94:8080"
+export const URL = "http://ec2-18-219-191-240.us-east-2.compute.amazonaws.com:8080"
 export const dataStatus = {
     NONE: 'NONE',
     SUCCESS: 'SUCCESS',
