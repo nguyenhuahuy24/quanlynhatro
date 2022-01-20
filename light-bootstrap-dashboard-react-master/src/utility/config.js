57e4fcec0f0fa238f7266505427f1033a661eefe
truong99 <truongnguyen30999@gmail.com>
2022-01-20 19:23:06 +0700

    deloy

--- light-bootstrap-dashboard-react-master/src/utility/config.js
+++ light-bootstrap-dashboard-react-master/src/utility/config.js
@@ -4,7 +4,7 @@ export const userProfile = {
     accessToken: "",
     userName: ""
 }
-export const URL = "http://3.95.159.254:8080"
+export const URL = "http://3.89.142.152:8080"
 export const dataStatus = {
     NONE: 'NONE',
     SUCCESS: 'SUCCESS',
